b85a744cbfdd043d89209a65bf01423cc08b4f2b
thib92 <thibhenry92@gmail.com>
2020-12-03 12:15:55 +0100

    Implement translocators recipes and generate Remove old recipes

--- src/main/java/codechicken/translocators/init/DataGenerators.java
+++ src/main/java/codechicken/translocators/init/DataGenerators.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Items;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -79,6 +80,38 @@ public class DataGenerators {
             ShapelessRecipeBuilder.shapelessRecipe(ModContent.diamondNuggetItem, 9)
                     .addIngredient(Items.DIAMOND)
                     .addCriterion("has_diamond", hasItem(Items.DIAMOND))
+                    .build(consumer);
+
+            ShapedRecipeBuilder.shapedRecipe(ModContent.itemTranslocatorItem, 2)
+                    .patternLine("RER")
+                    .patternLine("IPI")
+                    .patternLine("RGR")
+                    .key('R', Tags.Items.DUSTS_REDSTONE)
+                    .key('E', Tags.Items.ENDER_PEARLS)
+                    .key('I', Tags.Items.INGOTS_IRON)
+                    .key('P', Items.PISTON)
+                    .key('G', Tags.Items.INGOTS_GOLD)
+                    .addCriterion("has_redstone", hasItem(Tags.Items.DUSTS_REDSTONE))
+                    .addCriterion("has_ender_pearl", hasItem(Tags.Items.ENDER_PEARLS))
+                    .addCriterion("has_iron_ingot", hasItem(Tags.Items.INGOTS_IRON))
+                    .addCriterion("has_piston", hasItem(Items.PISTON))
+                    .addCriterion("has_gold_ingot", hasItem(Tags.Items.INGOTS_GOLD))
+                    .build(consumer);
+
+            ShapedRecipeBuilder.shapedRecipe(ModContent.fluidTranslocatorItem, 2)
+                    .patternLine("RER")
+                    .patternLine("IPI")
+                    .patternLine("RLR")
+                    .key('R', Tags.Items.DUSTS_REDSTONE)
+                    .key('E', Tags.Items.ENDER_PEARLS)
+                    .key('I', Tags.Items.INGOTS_IRON)
+                    .key('P', Items.PISTON)
+                    .key('L', Tags.Items.GEMS_LAPIS)
+                    .addCriterion("has_redstone", hasItem(Tags.Items.DUSTS_REDSTONE))
+                    .addCriterion("has_ender_pearl", hasItem(Tags.Items.ENDER_PEARLS))
+                    .addCriterion("has_iron_ingot", hasItem(Tags.Items.INGOTS_IRON))
+                    .addCriterion("has_piston", hasItem(Items.PISTON))
+                    .addCriterion("has_lapis", hasItem(Tags.Items.GEMS_LAPIS))
                     .build(consumer);
         }
     }
